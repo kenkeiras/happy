2c9360f2f36ba3c9f14f82a8356e62aefa117e27
kenkeiras <me@codigoparallevar.com>
2015-08-13 22:55:53 +0200

    Add valgrind check to test

--- test_rot13.sh
+++ test_rot13.sh
@@ -8,7 +8,7 @@ make
 
 # Simple test
 echo -e "\n\n\x1b[7mLet's try to cat\x1b[0m"
-time bin/happy dictionary $'flag stars are made of weird stuff'
+time valgrind bin/happy dictionary $'flag stars are made of weird stuff'
 
 # Little more complex
 echo -e "\n\n\x1b[7mThen let's add one\x1b[0m"
